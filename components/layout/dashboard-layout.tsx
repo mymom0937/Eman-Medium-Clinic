b790563ef7eb74451fd96dccbe91695afe95377f
mymom0937 <mymom0937@gmail.com>
2025-08-08 23:34:22 +0100

    Fixed the overlapped issues between navbar and dashboard pages

--- components/layout/dashboard-layout.tsx
+++ components/layout/dashboard-layout.tsx
@@ -27,10 +27,12 @@ export function DashboardLayout({
       {/* Main Content Area */}
       <div className="flex-1 flex flex-col ml-24 lg:ml-54">
         {/* Navbar */}
-        <div className="sticky top-0 z-40">
+        <div className="z-40">
           <Navbar />
+          {/* Spacer to offset the fixed navbar height */}
+          <div aria-hidden className="h-16 md:h-20" />
         </div>
-        <main className="flex-1 overflow-y-auto p-2 sm:p-4 md:p-6 pt-20">
+        <main className="flex-1 overflow-y-auto p-2 sm:p-4 md:p-6">
           <div className="max-w-full">{children}</div>
         </main>
       </div>
